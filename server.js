4e0809e0b4886076b8b0184832904cfa54e2da0e
Cavan Wagg <waggx002@gmail.com>
2018-06-23 08:36:57 -0500

    mongolab setup

--- server.js
+++ server.js
@@ -28,7 +28,10 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/webScraper");
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
 
 app.get("/", function(req, res) {
   
